66577e314a03bbd35acc6a878fa7a1827f8bf80a
rodogir <me@rodogir.com>
2019-12-26 19:29:29 +0100

    Add plugins & minimal configuration

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 module.exports = {
-  globals: {
-    MyGlobal: true
-  },
-
-  rules: {
-    semi: [2, "always"]
-  }
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react"
+  ]
 };
