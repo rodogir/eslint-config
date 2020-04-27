eba096449546ceac6c89f162d7443f884a8d1982
rodogir <me@rodogir.com>
2020-04-27 10:49:04 +0200

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,20 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: [
     "airbnb-typescript",
+    "airbnb/hooks",
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "prettier/@typescript-eslint",
     "prettier/react",
   ],
   rules: {
-    // hook rules
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
-
     // named exports are better supported by editors and refactoring tools
     "import/prefer-default-export": "off",
     "import/no-default-export": "warn",
 
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-empty-interface": "warn",
-    // anchor tags in Next.js never have a explicit href.
+    // anchor tags in Next.js never have an explicit href.
     "jsx-a11y/anchor-is-valid": [
       "error",
       {
